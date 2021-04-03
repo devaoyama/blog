5f9bf8f455453ced09614e240447a37cb8199cdd
devkeita <keita.mywork@gmail.com>
2021-04-04 00:21:37 +0900

    一覧を作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,54 @@
 import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Paginator from "../components/Paginator";
+import PostListItem from "../components/PostListItem";
+import { PostType } from '../types/PostType';
 
-export default function Index() {
+type IndexProps = {
+  posts: PostType[]
+};
+
+const Index: React.FC<IndexProps> = ({ posts }) => {
   return (
-    <div className={"text-center"}>
-      ブログ
-    </div>
+    <React.Fragment>
+      <Header />
+
+      <main className="min-h-screen">
+        <div className="container mx-auto flex flex-wrap py-6">
+
+          <section className="w-full flex flex-col items-center px-3">
+
+            {posts.map((post) => (
+              <PostListItem key={post.id} id={post.id} title={post.title} body={post.description} category={post.category} publishedAt={post.publishedAt} updatedAt={post.revisedAt} />
+            ))}
+
+          </section>
+
+          <Paginator prev="1" next="3" />
+        </div>
+      </main>
+
+      <Footer />
+      
+    </React.Fragment>
   );
 };
+
+export const getStaticProps = async () => {
+  const res = await fetch(process.env.MICRO_CMS_API_ENDPOINT + "/posts", {
+    method: "GET",
+    headers: {
+      "X-API-KEY": process.env.MICRO_CMS_API_KEY,
+    },
+  });
+  const data = await res.json();
+
+  return ({
+    props: {
+      posts: data.contents,
+    },
+  });
+};
+
+export default Index;

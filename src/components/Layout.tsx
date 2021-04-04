f366a3f7ddc3d5543d7d24ff8acb1bcdfe23161a
devkeita <keita.mywork@gmail.com>
2021-04-05 00:11:40 +0900

    Layoutコンポーネントを作成

---
+++ src/components/Layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Layout = ({ children }) => {
+  return (
+    <React.Fragment>
+      <Header />
+
+      <main>
+        {children}
+      </main>
+
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+export default Layout;

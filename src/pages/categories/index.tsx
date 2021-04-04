27134ce30f0465fe8673f64ee85d7f16724238d0
devkeita <keita.mywork@gmail.com>
2021-04-05 00:01:26 +0900

    カテゴリー一覧を作成

---
+++ src/pages/categories/index.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import React from "react";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import { CategoryType } from '../../types/CategoryType';
+
+type IndexProps = {
+  categories: CategoryType[]
+}
+
+const Index: React.FC<IndexProps> = ({ categories }) => {
+  return (
+    <React.Fragment>
+      <Header />
+
+      <main className="min-h-3/4 px-3">
+        <div className="container mx-auto w-full md:w-1/2 pl-5">
+          <h2 className="text-3xl font-bold my-5">カテゴリー</h2>
+
+          <ul className="list-disc ml-8">
+            {categories.map(category => (
+              <li key={category.id} className="text-lg my-2">
+                <Link href={`/categories/${category.id}`}>
+                  <span className="text-blue-600 cursor-pointer">
+                    {category.name}
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
+
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch(process.env.MICRO_CMS_API_ENDPOINT + "/categories", {
+    method: "GET",
+    headers: {
+      "X-API-KEY": process.env.MICRO_CMS_API_KEY,
+    },
+  });
+  const data = await res.json();
+
+  return ({
+    props: {
+      categories: data.contents,
+    },
+  });
+};
+
+export default Index;

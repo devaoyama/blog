c0c416d7ae00a6baab8503e6ca2b406e49534695
devkeita <keita.mywork@gmail.com>
2021-04-16 21:06:20 +0900

    Footerを修正

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-full border-t bg-white p-6">
       <div className="flex flex-col justify-center content-center">
-        <div className="uppercase text-center">&copy; <a href="https://twitter.com/IamK_prog" target="_blank">Keita Aoyama</a></div>
+        <div className="uppercase text-center">&copy; <a href="https://twitter.com/IamK_prog" target="_blank">Keita</a></div>
       </div>
     </footer>
   );
